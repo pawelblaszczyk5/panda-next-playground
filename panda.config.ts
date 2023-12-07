f00f16669c1bca6cf5bb90347d6ee1b858463b08
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2023-12-07 18:19:54 +0100

    Some hacks experiments

--- panda.config.ts
+++ panda.config.ts
@@ -7,6 +7,18 @@ export default defineConfig({
   theme: {
     extend: {},
   },
+  conditions: {
+    extend: {
+      test: "[data-test]",
+    },
+  },
+  hooks: {
+    "config:resolved": (config) => {
+      delete config.config.patterns;
+
+      console.log(config.config.conditions);
+    },
+  },
   strictTokens: true,
   outdir: "styled-system",
 });
